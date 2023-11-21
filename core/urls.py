d91cf0df0a8f12caf2d041cd94727d326c472a9c
Hemmerson Luis Barros da Rosa <hemmerson.rosa@gmail.com>
2023-11-21 11:21:05 -0300

    save, update and delete done

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,8 @@ from core import views
 
 urlpatterns = [
     path('', views.home),
+    path('salvar/', views.salvar, name='salvar'),
+    path('editar/<int:id>', views.editar, name='editar'),
+    path('update/<int:id>', views.update, name='update'),
+    path('delete/<int:id>', views.delete, name='deletar'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Pessoa
 
 def home(request):
     pessoas = Pessoa.objects.all()
-    return render(request, "index.html", {'pessoas': pessoas})
+    return render(request, "index.html", {'pessoas': pessoas, 'pessoa':''})
 
 def salvar(request):
-    pass
+    rNome = request.POST.get("nome")
+    rEmail = request.POST.get("email")
+    rIdade = int(request.POST.get("idade"))
+    Pessoa.objects.create(nome = rNome, email = rEmail, idade = rIdade)
+    pessoas = Pessoa.objects.all()
+    return render(request, "index.html", {'pessoas':pessoas})
+
+def editar(request, id):
+    pessoa = Pessoa.objects.get(id = id)
+    pessoas = Pessoa.objects.all()
+    return render(request, "index.html", {'pessoa':pessoa, 'pessoas':pessoas})
+
+def update(request, id):
+    pessoa = Pessoa.objects.get(id = id)
+    pessoa.nome = request.POST.get("nome")
+    pessoa.email = request.POST.get("email")
+    pessoa.idade = int(request.POST.get("idade"))
+    pessoa.save()
+    pessoas = Pessoa.objects.all()
+    return redirect(home)
+
+def delete(request, id):
+    pessoa = Pessoa.objects.get(id = id)
+    pessoa.delete()
+    return redirect(home)
